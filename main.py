4cb8b69aa1a636b698b3a03e9e1e8c8a4b59dbbf
daloncat <119846675+daloncat@users.noreply.github.com>
2023-04-13 06:37:49 +0000

    Added list and cleared an error that was caused by the os function

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import csv
-import os
-os.system("cls")
+import time
+from os import system, name
+
 
 # Define the name of the file
-filename = "Products.csv"
-totalsum = 0
+FILENAME = "Products.csv"
+TOTALSUM = 0
+
+def clear():
+	# for windows
+	if name == 'nt':
+		_ = system('cls')
+	# for mac and linux(here, os.name is 'posix')
+	else:
+		_ = system('clear')
 
 def searchProduct():
     product = input("Which Product are you searching?\n")
-    csv_file = csv.reader(open(filename), delimiter=';')
+    csv_file = csv.reader(open(FILENAME), delimiter=';')
     found_product = False
-    global totalsum
+    global TOTALSUM
 
     for row in csv_file:
         if len(row) >= 2 and product == row[1]:
@@ -22,11 +31,21 @@ def searchProduct():
                 gewicht = input("Please input the amount you wish: ")
                 summe = round(float(gewicht) * float(preiskg), 3)
                 print(summe)
-                totalsum = totalsum + summe
+                TOTALSUM = TOTALSUM + summe
+            elif relevance == "N":
+                summe = round(float(preis), 3)
+                TOTALSUM = TOTALSUM + summe
             found_product = True
     if not found_product:
         print("\033[31mProduct was not found in the database!\033[0m")
-            
+
+def listproducts():
+    print("We have following products in stock:")
+
+    csv_file = csv.reader(open(FILENAME), delimiter=';')
+    for row in csv_file:
+        print("\033[35m", row[1], "\033[35m")
+    time.sleep(2.5)
 
 #def searchCategory():
 #    product = input("Nach welcher Kategorie möchtest du ausschau halten?\n")
@@ -39,14 +58,17 @@ def searchProduct():
 #            found_category = True
 #
 #    if not found_category:
-#        print("\033[31mFehler\033[0m")
-            
+#        print("\033[31mFehler\033[0m")   
+
+clear()
+
 while True:
-    print("######################\nWelcome to our Shopping Simulator!\n######################\n")
+    print("\033[37m######################\nWelcome to our Shopping Simulator!\n######################\n\033[37m")
     print("\033[32mEnter 1 to search for Product Name:\033[0m")
 #    print("\033[32mEnter 2 to search for a category:\033[0m")
+    print("\033[32mEnter 3 to get a list of all items.\033[0m")
     print("\033[32mEnter 0 to quit.\033[0m")
-    print("\033[33mCurrent price of all products:\033[0m ", totalsum)
+    print("\033[33mCurrent price of all products:\033[0m ", TOTALSUM)
 
     src=int(input("Enter here: "))
 
@@ -54,6 +76,8 @@ while True:
         searchProduct()
 #    elif src==2:
 #        searchCategory()
+    elif src==3:
+        listproducts()
     elif src == 0:
         print("Thank you for shopping with us <3")
         break
